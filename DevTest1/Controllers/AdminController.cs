3b99bc788f7375d55d6289674ddb170c575ca96c
jhonmark <jhonmarkpaid@gmail.com>
2021-08-14 19:22:18 +0800

    initial commit

---
+++ DevTest1/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using DevTest1.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevTest1.Controllers
+{
+    public class AdminController : Controller
+    {
+        dev_test_dbEntities db = new dev_test_dbEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var res = db.users.ToList();
+
+            return View(res);
+        }
+    }
+}
---
+++ DevTest1/Controllers/ContactSubController.cs
@@ -0,0 +1,45 @@
+﻿using DevTest1.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevTest1.Controllers
+{
+    public class ContactSubController : Controller
+    {
+
+        dev_test_dbEntities db = new dev_test_dbEntities();
+
+        // GET: ContactSub
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContact(user model)
+        {
+            if (ModelState.IsValid)
+            {
+                user userObj = new user();
+                userObj.first_name = model.first_name;
+                userObj.last_name = model.last_name;
+                userObj.address = model.address;
+                userObj.state = model.state;
+                userObj.country = model.country;
+                userObj.zip_code = model.zip_code;
+                userObj.phone_number = model.phone_number;
+                userObj.note = model.note;
+
+                db.users.Add(userObj);
+                db.SaveChanges();
+            }
+
+            ModelState.Clear();
+
+            return View("Index");
+        }
+    }
+}
